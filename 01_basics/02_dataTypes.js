da89451a223dfd55805c22eafce3a6fb635dc796
Vinit Faldu <30413652+vinitfaldu@users.noreply.github.com>
2024-02-13 13:25:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+//alert("hello")
+//alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+
+ let name = "vinit"
+ let age = 24
+ let isLoggedIn = false
+ let state = null // define empty
+ let val // undefined 
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ //boolean => true/false
+ // numm standalone value
+ // undefined =>
+ //symbol => unique
+
+ // object
+
+ console.log(typeof "vinit") // string
+ console.log(typeof null) // object
+ console.log(typeof undefined) //undefinded
