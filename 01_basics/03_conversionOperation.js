1cf43048ab2b1629c6d28aaec5be691232bc9f4b
Vinit Faldu <30413652+vinitfaldu@users.noreply.github.com>
2024-02-15 09:45:55 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,15 @@ let str1 = "hello"
 let str2 = " vinit"
 
 let str3 = str1 + str2
-console.log(str3)
+// console.log(str3)
 
-console.log("1" + 2)
-console.log(1 + "2")
-console.log("1" + 2 + 2)
-console.log(1 + 2 + "2")
+// console.log("1" + 2)         // 12
+// console.log(1 + "2")         // 12
+// console.log("1" + 2 + 2)     // 122
+// console.log(1 + 2 + "2")     // 32
+
+console.log(true);      //true
+console.log(+true)      // 1
+// console.log(true+); //error
+console.log(+"");       // 0
+
