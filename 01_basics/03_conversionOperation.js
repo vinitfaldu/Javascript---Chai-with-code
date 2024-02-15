b2e52c40b32b653ca3633f227a40c2f286e3efb4
Vinit Faldu <30413652+vinitfaldu@users.noreply.github.com>
2024-02-15 09:35:13 +0000

    add details

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,3 +19,14 @@ console.log(booleanIsLoggedIn)
 
 // 1 => true
 // "vinit" => true, "" => false
+
+let str1 = "hello"
+let str2 = " vinit"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
